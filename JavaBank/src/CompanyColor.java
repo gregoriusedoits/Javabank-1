95e4c034bbec64d0ac1b3f358375493c0de3776d
gregoriusedoits <146185741+gregoriusedoits@users.noreply.github.com>
2023-11-09 10:18:03 +0700

    Add files via upload

---
+++ JavaBank/src/CompanyColor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gedos
+ */
+public class CompanyColor {
+    private final int R = 0;
+    private final int G = 169;
+    private final int B = 255;
+//this class uses the default
+//constructor provided by Java
+    public int getR() {
+    return R;
+    }//end method getR
+    public int getG() {
+    return G;
+    }//end method getG
+    public int getB() {
+    return B;
+    }//end method getB
+}//end class CompanyColor
+    
+
